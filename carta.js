0d319cb459617c343bc5afa3b3f59551999336f6
juli9894 <eze9894@gmail.com>
2026-05-15 19:28:56 -0300

    punto 2abc prueba

---
+++ carta.js
@@ -0,0 +1,34 @@
+class Carta {
+    constructor(code, value, suit, imagen) {
+        this.code = code;
+        this.value = value;
+        this.suit = suit;
+        this.imagen = imagen;
+    }
+
+    toJsonString() {
+        return JSON.stringify(this);
+    }
+
+    static createFromJsonString(json) {
+        const datos = JSON.parse(json);
+        return new Carta(
+            datos.code,
+            datos.value,
+            datos.suit,
+            datos.imagen,
+        );
+    }
+
+    createHtmlElement() {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+            <img src="${this.imagen}" alt="${this.code}" style="cursor:pointer">
+            <h5>${this.value}</h5> of ${this.suit}</h5>
+            <p>Código: ${this.code}</p>
+        `;
+
+        return div;
+    }
+}
